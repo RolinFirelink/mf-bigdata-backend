cf66b42994cf919b017b42f91113b73ec0f5b75d
cgli <cgli@qq.com>
2023-05-14 11:20:41 +0800

    去掉无用的表

---
+++ db/drop-table-2023-5-4.sql
@@ -0,0 +1,21 @@
+
+use mf_market;
+
+drop table t_data_tracer;
+drop table t_feedback;
+drop table t_goods;
+drop table t_help_doc;
+drop table t_help_doc_catalog;
+drop table t_help_doc_relation;
+drop table t_help_doc_view_record;
+drop table t_notice;
+drop table t_notice_type;
+drop table t_notice_view_record;
+drop table t_notice_visible_range;
+drop table t_oa_bank;
+drop table t_oa_enterprise;
+drop table t_oa_enterprise_employee;
+drop table t_oa_invoice;
+drop table t_reload_result;
+drop table t_serial_number;
+drop table t_serial_number_record;
