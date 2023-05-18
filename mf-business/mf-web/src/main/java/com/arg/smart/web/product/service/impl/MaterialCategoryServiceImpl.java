41e3bdee60a3ff12175e8a3bcef7afb20fe3080d
“Rolin” <2259286198@qq.com>
2023-05-18 13:58:31 +0800

    ✨ 产品类型CURD功能补充

---
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/product/service/impl/MaterialCategoryServiceImpl.java
@@ -0,0 +1,19 @@
+package com.arg.smart.web.product.service.impl;
+
+import com.arg.smart.web.product.entity.MaterialCategory;
+import com.arg.smart.web.product.mapper.MaterialCategoryMapper;
+import com.arg.smart.web.product.service.MaterialCategoryService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @description: 产品类型表
+ * @author cgli
+ * @date: 2023-05-18
+ * @version: V1.0.0
+ */
+@Service
+public class MaterialCategoryServiceImpl extends ServiceImpl<MaterialCategoryMapper, MaterialCategory> implements MaterialCategoryService {
+
+}
