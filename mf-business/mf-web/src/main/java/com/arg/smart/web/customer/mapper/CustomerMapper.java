b97bdb72e08bcdb77a1fe0a1d8d9b7481f3f5d19
rainkyzhong <rainkyzhong@sina.com>
2023-05-20 22:39:53 +0800

    fix:修复循环依赖的问题

--- mf-business/mf-web/src/main/java/com/arg/smart/web/customer/mapper/CustomerMapper.java
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/customer/mapper/CustomerMapper.java
@@ -2,6 +2,7 @@ package com.arg.smart.web.customer.mapper;
 
 import com.arg.smart.web.customer.entity.Customer;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @description: 客户表
@@ -9,6 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @date: 2023-05-17
  * @version: V1.0.0
  */
+@Mapper
 public interface CustomerMapper extends BaseMapper<Customer> {
 
 }
