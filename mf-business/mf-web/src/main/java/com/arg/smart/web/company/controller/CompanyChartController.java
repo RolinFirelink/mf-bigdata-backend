8a18fbdcdcbcdb0aecf37901f14cf79916f5ea9b
darkvoid <10629444+darkvoid@user.noreply.gitee.com>
2023-05-23 23:37:02 +0800

    拉取代码

---
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/company/controller/CompanyChartController.java
@@ -0,0 +1,21 @@
+package com.arg.smart.web.company.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: 企业报表数据接口
+ * @author zsj
+ * @date: 2023-05-21
+ * @version: V1.0.0
+ */
+@Slf4j
+@Api(tags = "企业报表数据接口")
+@RestController
+@RequestMapping("/companyChart")
+public class CompanyChartController {
+
+
+}
