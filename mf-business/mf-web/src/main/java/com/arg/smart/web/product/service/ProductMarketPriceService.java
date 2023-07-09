be7a1b9018c0ed478c570db753acd0c7085e5e45
“Rolin” <2259286198@qq.com>
2023-07-09 16:31:57 +0800

    🎨 添加批量价格基础内容并增加爬取数据的接口

---
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/product/service/ProductMarketPriceService.java
@@ -0,0 +1,19 @@
+package com.arg.smart.web.product.service;
+
+import com.arg.smart.web.product.entity.ProductMarketPrice;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @description: 产品批发价格表
+ * @author cgli
+ * @date: 2023-07-08
+ * @version: V1.0.0
+ */
+public interface ProductMarketPriceService extends IService<ProductMarketPrice> {
+
+    /**
+     * 通过爬虫将网站中的数据添加到数据库中
+     * @return
+     */
+    boolean crawlerSave();
+}
