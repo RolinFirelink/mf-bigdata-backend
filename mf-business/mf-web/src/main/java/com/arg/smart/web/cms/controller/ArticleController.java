4a0fd7af2c675316737cedcea38a0e0acabb3658
darkvoid <10629444+darkvoid@user.noreply.gitee.com>
2023-07-26 20:11:14 +0800

    mergr XTC

--- mf-business/mf-web/src/main/java/com/arg/smart/web/cms/controller/ArticleController.java
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/cms/controller/ArticleController.java
@@ -50,7 +50,7 @@ public class ArticleController {
      */
     @Log(title = "从ES中查询数据返回给前端", operateType = OperateType.INSERT)
     @ApiOperation("从ES中查询数据返回给前端")
-    @GetMapping("/public/{content}")
+    @GetMapping("/public/search/{content}")
     public Result<List<Article>> getArticlesByEs(@PathVariable("content") String content) {
         return Result.ok(articleInfoService.findArticlesByEs(content), "ES文章内容-查询成功!");
     }
