f9c007c881380277ddc150c145e8ae7b1b93dee9
“Rolin” <2259286198@qq.com>
2023-07-29 14:15:46 +0800

    🎨 惠农网采购数量数据爬虫开发完毕

---
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/product/entity/ProductMarketNums.java
@@ -0,0 +1,42 @@
+package com.arg.smart.web.product.entity;
+
+import com.arg.smart.common.core.entity.BaseEntity;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * @description: 批发市场采购量表
+ * @author cgli
+ * @date: 2023-07-29
+ * @version: V1.0.0
+ */
+@Data
+@TableName("sh_product_market_nums")
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@ApiModel(value = "sh_product_market_nums对象", description = "批发市场采购量表")
+public class ProductMarketNums extends BaseEntity<Long> {
+    @ApiModelProperty(value = "唯一ID")
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    @ApiModelProperty(value = "产品名")
+	private String name;
+    @ApiModelProperty(value = "区分字段")
+	private Integer flag;
+    @ApiModelProperty(value = "采购数量")
+	private Double purchaseNums;
+    @ApiModelProperty(value = "期望货源地")
+	private String expectedSource;
+    @ApiModelProperty(value = "发布人")
+	private String publisher;
+    @ApiModelProperty(value = "认证情况")
+	private String authenticationStatus;
+    @ApiModelProperty(value = "计量单位")
+	private String unit;
+}
---
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/product/mapper/ProductMarketNumsMapper.java
@@ -0,0 +1,14 @@
+package com.arg.smart.web.product.mapper;
+
+import com.arg.smart.web.product.entity.ProductMarketNums;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @description: 批发市场采购量表
+ * @author cgli
+ * @date: 2023-07-29
+ * @version: V1.0.0
+ */
+public interface ProductMarketNumsMapper extends BaseMapper<ProductMarketNums> {
+
+}
