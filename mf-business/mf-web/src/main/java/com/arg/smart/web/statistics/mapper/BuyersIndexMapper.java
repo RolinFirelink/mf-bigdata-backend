14599fa7e65e178e8e3cdb28001b118bee081be5
GlowingTree <3020730747@qq.com>
2023-07-31 18:08:13 +0800

    fix: 优化获取采购商指数接口

--- mf-business/mf-web/src/main/java/com/arg/smart/web/statistics/mapper/BuyersIndexMapper.java
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/statistics/mapper/BuyersIndexMapper.java
@@ -1,6 +1,7 @@
 package com.arg.smart.web.statistics.mapper;
 
 import com.arg.smart.web.statistics.entity.BuyersIndex;
+import com.arg.smart.web.statistics.vo.BuyerPurchaseVO;
 import com.arg.smart.web.statistics.vo.BuyersIndexVO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Param;
@@ -25,12 +26,21 @@ public interface BuyersIndexMapper extends BaseMapper<BuyersIndex> {
 	List<String> selectBuyersName(@Param("flag") Integer flag, @Param("year") Integer year, @Param("month") Integer month);
 
 	/**
-	 * 获取某模块下所有采购商名称
+	 * 获取某模块下一个采购商的采购商指数
 	 * @param flag 模块名称
 	 * @param year 年
 	 * @param month 月
 	 * @return BuyersIndexVo
 	 */
-	BuyersIndexVO selectOneBuyerIndex(@Param("flag") Integer flag, @Param("name") String name, @Param("year") Integer year, @Param("month") Integer month);
+	BuyersIndexVO selectOneBuyerIndex(@Param("flag") Integer flag,
+	                                  @Param("name") String name,
+	                                  @Param("year") Integer year,
+	                                  @Param("month") Integer month);
+
+
+	BuyerPurchaseVO selectOneBuyerPurchaseInfo(@Param("flag") Integer flag,
+	                                           @Param("name") String name,
+	                                           @Param("year") Integer year,
+	                                           @Param("month") Integer month);
 
 }
---
+++ mf-business/mf-web/src/main/java/com/arg/smart/web/statistics/vo/BuyerPurchaseVO.java
@@ -0,0 +1,28 @@
+package com.arg.smart.web.statistics.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * @Author GlowingTree
+ * @Description 采购商某月采购数据值对象
+ * @Date 7/31/2023 5:12 PM
+ * @Version 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@ApiModel(value = "采购商某月采购数据", description = "采购商某月采购数据")
+@Builder
+public class BuyerPurchaseVO {
+	@ApiModelProperty("采购商名称")
+	private String publisher;
+	@ApiModelProperty("本月采购总价")
+	private Double totalPrice;
+}
